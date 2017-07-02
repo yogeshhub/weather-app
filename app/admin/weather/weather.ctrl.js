a475100c68b728f5c5de54172e9aaeff58de3025
Yogesh <yogimayi@outlook.com>
2017-07-02 21:40:54 +0530

    weather design completed

--- app/admin/weather/weather.ctrl.js
+++ app/admin/weather/weather.ctrl.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('weatherApp')
-	.controller('weatherCtrl', [function() {
-		
+	.controller('weatherCtrl', [function($scope) {
+		var wc = this;
+		wc.exampleLocations = [
+        	{ title:'Hamburg'},
+        	{ title:'San Francisco'},
+        	{ title:'Berlin'},
+        	{ title:'Toronto'},
+        	{ title:'Athens'},
+        	{ title:'chennai'}
+        ];        
 	}]);
