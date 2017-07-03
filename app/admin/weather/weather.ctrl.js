a6391b0dc076065aece361e10be628628bae5432
Yogesh <yogimayi@outlook.com>
2017-07-03 19:45:06 +0530

    Weather and admin has done

--- app/admin/weather/weather.ctrl.js
+++ app/admin/weather/weather.ctrl.js
@@ -1,14 +1,35 @@
 'use strict';
 
 angular.module('weatherApp')
-	.controller('weatherCtrl', [function($scope) {
+	.controller('weatherCtrl', ['$scope','openWeatherMap','$mdDialog',function($scope,openWeatherMap,$mdDialog) {
 		var wc = this;
-		wc.exampleLocations = [
-        	{ title:'Hamburg'},
-        	{ title:'San Francisco'},
-        	{ title:'Berlin'},
-        	{ title:'Toronto'},
-        	{ title:'Athens'},
-        	{ title:'chennai'}
-        ];        
+		wc.exampleLocations = ['London','Toronto','Chennai','New York'];        
+    wc.iconBaseUrl = 'http://openweathermap.org/img/w/';
+
+    angular.forEach(wc.exampleLocations, function(value, key){
+        openWeatherMap.getWeather(value).then(function(data){
+          wc.exampleLocations[key] = data;
+        });
+    });   
+
+    wc.getImageSrc = function(iconName){
+      return (iconName ? wc.iconBaseUrl + iconName + '.png' : '');
+    } 
+
+    wc.getEachWeather = function(place){      
+      openWeatherMap.getWeather(place).then(function(data){
+          wc.exampleLocations.push(data);
+        },function(err){
+          $mdDialog.show(
+            $mdDialog.alert()
+              .parent(angular.element(document.querySelector('#weatherPortion')))
+              .clickOutsideToClose(true)
+              .title('Ooopss!!')
+              .textContent('Please Enter place name correctly..')
+              .ariaLabel('Alert Dialog Demo')
+              .ok('Got it!')
+          );
+        });
+    }
+
 	}]);
--- app/components/service/open_weather.service.js
+++ app/components/service/open_weather.service.js
@@ -2,53 +2,34 @@
 
 angular.module('weatherApp')
 
-.factory('openWeatherMap', function($resource) {
+.factory('openWeatherMap', function($http,$q) {
 
     // API key is currently unused (work either with or without key)
     var apiKey = '279b4be6d54c8bf6ea9b12275a567156';
     var apiBaseUrl = 'http://api.openweathermap.org/data/2.5/';
+    return{
+      getWeather: function(location){  
+           var defer = $q.defer();
+          var request = {
+            method: 'GET',
+            url: apiBaseUrl + 'weather',
+            params: {
+               q: location,
+              mode: 'json',
+              units: 'metric',
+              cnt: '7',
+              appid: '279b4be6d54c8bf6ea9b12275a567156'
+            }
+          };
 
-    return $resource(apiBaseUrl + ':path/:subPath?q=:location',
-      {
-        APPID: apiKey,
-        mode: 'json',
-        callback: 'JSON_CALLBACK',
-        units: 'metric',
-        lang: 'en'
-      },
-      {
-        queryWeather: {
-          method: 'JSONP',
-          params: {
-            path: 'weather'
-          },
-          isArray: false,
-          headers: {
-            'x-api-key': apiKey
-          }
-        },
-        queryForecast: {
-          method: 'JSONP',
-          params: {
-            path: 'forecast'
-          },
-          isArray: false,
-          headers: {
-            'x-api-key': apiKey
-          }
-        },
-        queryForecastDaily: {
-          method: 'JSONP',
-          params: {
-            path: 'forecast',
-            subPath: 'daily',
-            cnt: 7
-          },
-          isArray: false,
-          headers: {
-            'x-api-key': apiKey
-          }
-        }
+          $http(request)
+            .then(function(response) {
+              defer.resolve(response.data);
+            }).
+            catch(function(err) {
+              defer.reject(err);
+            });         
+            return defer.promise;   
       }
-    )
+    }    
   });
